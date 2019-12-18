d3125a5ef04d09159b5953c8efe7fffa8885f87e
André Caiado <andrecaiado@gmail.com>
2019-12-18 21:38:58 +0000

    Initial commit.

---
+++ src/test/java/pt/com/andrecaiado/restwithspringbootudemy/RestWithSpringbootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.com.andrecaiado.restwithspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

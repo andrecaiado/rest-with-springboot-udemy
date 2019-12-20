031c7f397ea10987dcb2f2cab68bada161cf3f00
André Caiado <andrecaiado@gmail.com>
2019-12-20 20:09:50 +0000

    Chapter-9 done

---
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `person` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address` varchar(100) NOT NULL,
+  `first_name` varchar(80) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
---
+++ src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` VALUES 
+	(1,'Rua Mestre António Nelas, Lote 191-A, 3º esq., Urbanização Quinta do Bosque, 3510-596, Viseu','Maria','Female','Flor'),
+	(3,'Rua Mestre António Nelas, Lote 191-A, 3º esq., 3510-596, Viseu','André','Male','Caiado'),
+	(6,'Rua Mestre António Nelas, Lote 191-A, 3º esq., 3510-596, Viseu','Carla','Male','Frias11'),
+	(7,'Rua Mestre António Nelas, Lote 191-A, 3º esq., 3510-596, Viseu','Carla','Male','Frias11');

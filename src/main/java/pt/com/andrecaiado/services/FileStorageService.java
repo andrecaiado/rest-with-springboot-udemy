5d96600367346d4589864d8a6127144bb76e531f
André Caiado <andrecaiado@gmail.com>
2019-12-31 01:44:15 +0000

    Download done

--- src/main/java/pt/com/andrecaiado/services/FileStorageService.java
+++ src/main/java/pt/com/andrecaiado/services/FileStorageService.java
@@ -6,12 +6,15 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import pt.com.andrecaiado.config.FileStorageConfig;
 import pt.com.andrecaiado.exceptions.FileStorageException;
+import pt.com.andrecaiado.exceptions.MyFileNotFoundException;
 
 @Service
 public class FileStorageService {
@@ -45,6 +48,19 @@ public class FileStorageService {
 			return fileName;
 		} catch (Exception e) {
 			throw new FileStorageException("Could not store the file " + fileName + ". Please try again.", e);
+		}
+	}
+	
+	public Resource loadFileAsResource(String fileName) {
+		try {
+			Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+			Resource resource = new UrlResource(filePath.toUri());
+			if (resource.exists()) {
+				return resource;
+			}
+			throw new MyFileNotFoundException("File not found + " + fileName);
+		} catch (Exception e) {
+			throw new MyFileNotFoundException("File not found + " + fileName, e);
 		}
 	}
 	
